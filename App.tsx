b7c5a37cb667bf0ca575e8a54ead4d6606e181fe
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-09 17:06:55 -0300

    :wrench: Add Font and Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,26 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
 
 import { Widget } from './src/components/Widget';
 
 import { theme } from './src/theme';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: theme.colors.background
-    }}
-    >
-      <Widget />
+  const [appIsReady, setAppIsReady] = useState(false);
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{ flex: 1, backgroundColor: theme.colors.background }}
+      onLayout={onLayoutRootView}
+    >
       <StatusBar
         style="light"
-        backgroundColor='transparent'
+        backgroundColor="transparent"
         translucent
       />
+
+      <Widget />
     </View>
   );
 }
