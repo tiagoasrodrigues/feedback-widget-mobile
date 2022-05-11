36699bf6d41834807676963caa102b32f2d224e7
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-10 21:43:48 -0300

    :wrench: Componente Option

--- src/components/Option/styles.ts
+++ src/components/Option/styles.ts
@@ -1,7 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
+  container: {},
+  image: {
+
+  },
+  title: {
+    
   }
 });
