c98dadb4bae61868341a0067ebe8fac2ef457ce1
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-12 10:30:17 -0300

    :wrench: Finalizando componente Success

--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+import { Copyright } from '../Copyright';
+
+import successImg from '../../assets/success.png'
 
 import { styles } from './styles';
 
 export function Success() {
   return (
     <View style={styles.container}>
+      <Image
+        source={successImg}
+        style={styles.image}
+      />
 
+      <Text style={styles.title}>
+        Agradecemos o feedback
+      </Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonTitle}>
+          Quero enviar outro
+        </Text>
+      </TouchableOpacity>
+
+      <Copyright />
     </View>
   );
 }
