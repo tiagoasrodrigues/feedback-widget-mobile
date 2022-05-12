6c86ec493751bfacae0ec7bb0128a7962d5352d1
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-12 12:03:58 -0300

    :wrench: Integrando o back-end utilizando Axios

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Image,
   Text,
@@ -6,28 +6,74 @@ import {
   TouchableOpacity,
   View
 } from 'react-native';
+import { captureScreen } from 'react-native-view-shot';
 import { ArrowLeft } from 'phosphor-react-native';
+import * as FileSystem from 'expo-file-system';
 
-import { FeedbackType } from '../Widget'
-import { ScreenshotButton } from '../ScreenshotButton'
-import { Button } from '../Button'
+import { FeedbackType } from '../Widget';
+import { ScreenshotButton } from '../ScreenshotButton';
+import { Button } from '../Button';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
 
-import { feedbackTypes } from '../../utils/feedbackTypes'
+import { api } from '../../libs/api';
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
 interface Props {
   feedbackType: FeedbackType;
+  onFeedbackCanceled: () => void;
+  onFeedbackSent: () => void;
 }
 
-export function Form({ feedbackType }: Props) {
+export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props) {
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState("");
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleScreenshot() {
+    captureScreen({
+      format: 'jpg',
+      quality: 0.8
+    })
+      .then(uri => setScreenshot(uri))
+      .catch(error => console.log(error));
+  }
+
+  function handleScreenshotRemove() {
+    setScreenshot(null);
+  }
+
+  async function handleSendFeedback() {
+    if (isSendingFeedback) {
+      return;
+    }
+    setIsSendingFeedback(true);
+    const screenshotBase64 = screenshot && await FileSystem.readAsStringAsync(screenshot, {encoding: 'base64'});
+
+    try {
+      await api.post('/feedbacks', {
+        type: feedbackType,
+        screenshot: `data:image/png;base64, ${screenshotBase64}`,
+        comment
+      });
+
+      onFeedbackSent();
+
+    } catch (error) {
+      console.log(error);
+      setIsSendingFeedback(false);
+    }
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={onFeedbackCanceled}
+        >
           <ArrowLeft
             size={24}
             weight="bold"
@@ -51,16 +97,19 @@ export function Form({ feedbackType }: Props) {
         style={styles.input}
         placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
         placeholderTextColor={theme.colors.text_secondary}
+        autoCorrect={false}
+        onChangeText={setComment}
       />
       <View style={styles.footer}>
-        
-        <ScreenshotButton 
-          onTakeShot={() => {}}
-          onRemoveShot={() => {}}
-          screenshot="https://github.com/tiagoasrodrigues.png"
+
+        <ScreenshotButton
+          onTakeShot={handleScreenshot}
+          onRemoveShot={handleScreenshotRemove}
+          screenshot={screenshot}
         />
         <Button
-          isLoading={false}
+          onPress={handleSendFeedback}
+          isLoading={isSendingFeedback}
         />
       </View>
 
--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -12,7 +12,11 @@ import successImg from '../../assets/success.png'
 
 import { styles } from './styles';
 
-export function Success() {
+interface Props {
+  onSendAnotherFeedback: () => void;
+}
+
+export function Success({ onSendAnotherFeedback }: Props) {
   return (
     <View style={styles.container}>
       <Image
@@ -24,7 +28,10 @@ export function Success() {
         Agradecemos o feedback
       </Text>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity 
+      style={styles.button}
+      onPress={onSendAnotherFeedback}
+      >
         <Text style={styles.buttonTitle}>
           Quero enviar outro
         </Text>
