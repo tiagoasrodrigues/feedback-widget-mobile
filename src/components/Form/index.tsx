2431902a4a0eab23625dc3635b3001b5ceba3d97
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-12 09:40:10 -0300

    :wrench: Componente Button

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -9,6 +9,7 @@ import {
 import { ArrowLeft } from 'phosphor-react-native';
 
 import { FeedbackType } from '../Widget'
+import { ScreenshotButton } from '../ScreenshotButton'
 
 import { styles } from './styles';
 import { theme } from '../../theme';
@@ -50,6 +51,13 @@ export function Form({ feedbackType }: Props) {
         placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
         placeholderTextColor={theme.colors.text_secondary}
       />
+      <View style={styles.footer}>
+        <ScreenshotButton 
+          onTakeShot={() => {}}
+          onRemoveShot={() => {}}
+          screenshot=""
+        />
+      </View>
 
     </View>
   );
--- src/components/ScreenshotButton/index.tsx
+++ src/components/ScreenshotButton/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  Image,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Camera, Trash } from 'phosphor-react-native';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
 
-export function ScreenshotButton() {
+interface Props {
+  screenshot: string | null;
+  onTakeShot: () => void;
+  onRemoveShot: () => void;
+}
+
+export function ScreenshotButton({ screenshot, onTakeShot, onRemoveShot }: Props) {
   return (
-    <View style={styles.container}>
-
-    </View>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={screenshot ? onRemoveShot : onTakeShot}
+    >
+      {
+        screenshot
+          ?
+          <Trash
+            size={22}
+            color={theme.colors.text_secondary}
+            weight="fill"
+            style={styles.removeIcon}
+          />
+          :
+          <Camera 
+          size={24}
+          color={theme.colors.text_primary}
+          weight="bold"
+          />
+      }
+    </TouchableOpacity>
   );
 }
